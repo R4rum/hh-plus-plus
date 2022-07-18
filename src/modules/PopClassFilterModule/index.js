b663d1da5aa9b7221540d292a357b78e9054199c
Rarum <thesagely@gmail.com>
2022-07-18 20:12:49 +0300

    Improved the PoP class filter, and reduced the jankiness reload button location.

--- src/modules/PopClassFilterModule/index.js
+++ src/modules/PopClassFilterModule/index.js
@@ -1,14 +1,16 @@
 import CoreModule from '../CoreModule'
 import Helpers from '../../common/Helpers'
 import I18n from '../../i18n'
-import filterIcon from '../../assets/filter.svg'
-import { lsKeys } from '../../common/Constants'
 
 import styles from './styles.lazy.scss'
 import Sheet from '../../common/Sheet'
 
 const {$} = Helpers
 const MODULE_KEY = 'popClassFilter'
+
+const createFilterBtn = (className) => {
+    return $('<label class="' + className + '"><input type="button" class="blue_button_L" value="" /></label>')
+}
 
 // Original code by Rarum
 class PopClassFilterModule extends CoreModule {
@@ -22,27 +24,34 @@ class PopClassFilterModule extends CoreModule {
 
     }
 
-    applyFilter (currentPoPId) {
+    applyFilter (currentPoPId, wanted_class) {
         if (!currentPoPId) {return}
 
         const {pop_data, pop_hero_girls} = window
 
         const currentPoP = pop_data[currentPoPId]
         const {status, girls} = currentPoP
-        const wanted_class = currentPoP.class
-
-        if (status !== 'can_start') {return}
-
         const $girlsContainer = $('.pop_right_part .grid_view')
 
         girls.forEach(({id_girl}) => {
-            const {class: carac} = pop_hero_girls[id_girl]
-
-            if (carac != wanted_class) {
-                const $girl = $girlsContainer.find(`[girl=${id_girl}]`)
-                $girl.addClass('filtered_out')
-            }
+            const $girl = $girlsContainer.find(`[girl=${id_girl}]`)
+            $girl.removeClass('filtered_out')
         })
+
+        if (wanted_class == 0)  wanted_class = currentPoP.class;
+
+        if (status !== 'can_start') {return}
+
+        if (wanted_class >= 0) {
+            girls.forEach(({id_girl}) => {
+                const {class: carac} = pop_hero_girls[id_girl]
+
+                if (carac != wanted_class) {
+                    const $girl = $girlsContainer.find(`[girl=${id_girl}]`)
+                    $girl.addClass('filtered_out')
+                }
+            })
+        }
     }
 
     shouldRun () {
@@ -61,16 +70,41 @@ class PopClassFilterModule extends CoreModule {
                 const currentPoPId = searchParams.get('index')
 
                 if (currentPoPId) {
-                    this.applyFilter(currentPoPId)
+                    this.injectCSSVars()
+                    const $container = $('.pop_right_part .grid_view')
+                    const $no_btn = createFilterBtn("no_filter")
+                    const $hc_btn = createFilterBtn("hc_filter")
+                    const $ch_btn = createFilterBtn("ch_filter")
+                    const $kh_btn = createFilterBtn("kh_filter")
+                    $container.append($no_btn).append($hc_btn).append($ch_btn).append($kh_btn)
+                    $no_btn.click(() => this.applyFilter(currentPoPId, -1))
+                    $hc_btn.click(() => this.applyFilter(currentPoPId, 1))
+                    $ch_btn.click(() => this.applyFilter(currentPoPId, 2))
+                    $kh_btn.click(() => this.applyFilter(currentPoPId, 3))
+
+                    this.applyFilter(currentPoPId, 0)
 
                     new MutationObserver(() => {
-                        this.applyFilter(currentPoPId)
+                        this.applyFilter(currentPoPId, 0)
                     }).observe($('.pop_right_part .grid_view')[0], {childList: true})
+
+                    // Catch any button press, because hacky tweaks.
+                    $(document).click(function(){
+                        const $container = $('.pop_right_part .grid_view')
+                        $container.getNiceScroll().resize()
+                    }); 
                 }
             }
         })
 
         this.hasRun = true
+    }
+
+    injectCSSVars() {
+        Sheet.registerVar('no-icon', `url('${Helpers.getCDNHost()}/pictures/misc/items_icons/16.svg')`)
+        Sheet.registerVar('hc-icon', `url('${Helpers.getCDNHost()}/pictures/misc/items_icons/1.svg')`)
+        Sheet.registerVar('ch-icon', `url('${Helpers.getCDNHost()}/pictures/misc/items_icons/2.svg')`)
+        Sheet.registerVar('kh-icon', `url('${Helpers.getCDNHost()}/pictures/misc/items_icons/3.svg')`)
     }
 }
 
